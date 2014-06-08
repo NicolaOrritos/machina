60853e0a5b5788dcf71a3f6fb8b8a8f2a1ef7657
Nicola Orritos <nicola.orritos@gmail.com>
2014-06-08 17:26:23 +0200

    Fixed some JSHint warnings

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,9 @@ module.exports = function (grunt) {
             gruntfile: {
                 src: 'Gruntfile.js'
             },
+            bin: {
+                src: ['bin/**/*']
+            },
             lib: {
                 src: ['lib/**/*.js']
             },
