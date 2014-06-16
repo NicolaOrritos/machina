5afc46863ad331ffbc8dd3195205bf654b227d8c
Nicola Orritos <nicola.orritos@gmail.com>
2014-06-16 15:26:01 +0200

    Added Parser.parseSync() plus the code (and demo) that completes the ASUB-AACK-TBEGIN roundtrip

--- test/fakeagent.js
+++ test/fakeagent.js
@@ -11,6 +11,10 @@ client.ASUB('fakeagent', [], true).on(commands.AACK, function(metadata)
 {
     console.log('Received AACK for ASUB. Metadata: %s', JSON.stringify(metadata));
     
+}).on(commands.TBEGIN, function(metadata)
+{
+    console.log('Received TBEGIN. Metadata: %s', JSON.stringify(metadata));
+    
 }).on(commands.ERROR, function(cause)
 {
     console.log('Received ERROR for ASUB: %s', cause);
